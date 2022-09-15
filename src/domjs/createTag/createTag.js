cd96356a63c57bd1c7fdc5415aceb49931fd847a
Alejandro Scotti <ascotti@hastingsmutual.com>
2022-09-14 20:35:31 -0400

    Add functionality to createTag(). Create new build.

--- src/domjs/createTag/createTag.js
+++ src/domjs/createTag/createTag.js
@@ -1,35 +1,50 @@
-dom.createTag = (name, config) => {
-	if (!name || name === "") {
-		console.error("createTag() 'name argument is missing --> config: ", config);
+dom.createTag = (nameOrConfig, config) => {
+	const utils = dom.utils;
+	let m_props;
+
+	if (!nameOrConfig || nameOrConfig === "") {
+		console.error("createTag() 'required first argument is missing a value. Second argument --> ", config);
 		return;
 	}
 
-	const utils = dom.utils;
-	const newEl = utils.createTagNS(name);
+	if (utils.isObject(nameOrConfig)) {
+		if (!nameOrConfig.name) {
+			console.error("createTag() 'object is missing the key 'name' with string value tag name. Object --> ", nameOrConfig);
+			return;
+		}
 
-	if (!config || !utils.isObject(config)) {
-		return newEl;
+		if (!utils.isString(nameOrConfig.name) || nameOrConfig.name === "") {
+			console.error("createTag() 'object key 'name' must be string value tag name. Object --> ", nameOrConfig);
+			return;
+		}
+
+		m_props = { ...nameOrConfig };
+	} else {
+		m_props = { ...config };
+		m_props.name = nameOrConfig;
 	}
 
-	switch (name) {
+	const newEl = utils.createTagNS(m_props.name);
+
+	switch (m_props.name) {
 		case "input":
 		case "textarea":
 		case "select":
 		case "option":
 		case "output":
-			newEl.value = config.text ? config.text : "";
+			newEl.value = m_props.text ? m_props.text : "";
 			break;
 		default:
-			newEl.innerText = config.text ? config.text : "";
+			newEl.innerText = m_props.text ? m_props.text : "";
 			break;
 	}
 
-	dom.setProps(newEl, config.prop);
-	dom.setAttr(newEl, config.attr);
-	utils.setEvent(newEl, config.event);
+	dom.setProps(newEl, m_props.prop);
+	dom.setAttr(newEl, m_props.attr);
+	utils.setEvent(newEl, m_props.event);
 
-	if (config.class && config.class !== "") {
-		newEl.className = config.class;
+	if (m_props.class && m_props.class !== "") {
+		newEl.className = m_props.class;
 	}
 
 	return newEl;
